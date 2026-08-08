4ab277295fc834f2dbb98698c436c14d7a9818fb
Jose Luis Pereira <jl24pereira@gmail.com>
2026-08-08 16:59:03 -0600

    feat: Configuraciones de spring
    Se configuran perfiles para ambientes
    Se configura actuator con health, info y metricas
    Se agregan librerias para validacion de token en build.gradle

--- api/src/main/java/com/pereira/api/ApiApplication.java
+++ api/src/main/java/com/pereira/api/ApiApplication.java
@@ -2,12 +2,14 @@ package com.pereira.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 
 }
