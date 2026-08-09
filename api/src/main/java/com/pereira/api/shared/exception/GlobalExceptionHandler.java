c7e7f41dd259468bbe4c9768bde4a39f48c50249
Jose Luis Pereira <jl24pereira@gmail.com>
2026-08-09 16:22:26 -0600

    feat: Actualizacion de GlobalExceptionHandler para manejo de errores de ESPACIO

--- api/src/main/java/com/pereira/api/shared/exception/GlobalExceptionHandler.java
+++ api/src/main/java/com/pereira/api/shared/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -35,5 +36,21 @@ public class GlobalExceptionHandler {
         problem.setDetail("Error de validacion");
         problem.setProperty("errores", errors);
         return problem;
+    }
+
+    @ExceptionHandler(EspacioNotFoundException.class)
+    ProblemDetail espacioNotFound(EspacioNotFoundException e) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    ProblemDetail invalidBody(HttpMessageNotReadableException e) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST,
+                "El BODY de la peticion no es valido o contiene valores no permitidos");
+    }
+
+    @ExceptionHandler(Exception.class)
+    ProblemDetail generic(Exception e) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, "Ha ocurrido un error inesperado");
     }
 }
